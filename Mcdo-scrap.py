aca5b1ef50e27a04d1b6974b0b775b4b87bafac9
Toki <yassineferhane69001@hotmail.com>
2024-04-14 18:34:12 +0200

    store google data

--- Mcdo-scrap.py
+++ Mcdo-scrap.py
@@ -13,3 +13,24 @@ def get_mcdonalds_in_paris(api_key):
     }
 
     locations = []
+
+        while True:
+        response = requests.get(url, params=params)
+        data = response.json()
+
+        if "results" in data:
+            for place in data["results"]:
+                locations.append({
+                    "name": place.get("name"),
+                    "address": place.get("formatted_address"),
+                    "lat": place["geometry"]["location"].get("lat"),
+                    "lng": place["geometry"]["location"].get("lng"),
+                })
+
+        # Check if there is another page of results
+        if "next_page_token" in data:
+            params["pagetoken"] = data["next_page_token"]
+        else:
+            break
+
+    return locations
